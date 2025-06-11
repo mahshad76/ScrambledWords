11d09b12a4352a2d10ee0de6b0af9885d11a8f9c
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-11 16:04:03 -0400

    adding score and corectness checking bool to the state data class

--- app/src/main/java/com/example/unscramble/ui/uistatemodels/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/uistatemodels/GameUiState.kt
@@ -1,5 +1,7 @@
 package com.example.unscramble.ui.uistatemodels
 
 data class GameUiState(
-    val currentScrambledWord: String = ""
+    val currentScrambledWord: String = "",
+    val isGuessCorrect: Boolean = false,
+    val score: Int = 0
 )
