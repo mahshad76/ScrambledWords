c96993a13f0918ddbef37bbaa663c4b8c2856299
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-13 12:39:22 -0400

    checkUserGuess

--- app/src/main/java/com/example/unscramble/ui/uistatemodels/GameUiState.kt
+++ app/src/main/java/com/example/unscramble/ui/uistatemodels/GameUiState.kt
@@ -2,6 +2,6 @@ package com.example.unscramble.ui.uistatemodels
 
 data class GameUiState(
     val currentScrambledWord: String = "",
-    val isGuessCorrect: Boolean = false,
+    val isGuessCorrect: Boolean = true,
     val score: Int = 0
 )
--- app/src/main/java/com/example/unscramble/ui/viewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/viewModel.kt
@@ -1,12 +1,17 @@
 package com.example.unscramble.ui
 
 import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.unscramble.data.allWords
 import com.example.unscramble.ui.uistatemodels.GameUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 
 class GameViewModel : ViewModel() {
@@ -15,6 +20,9 @@ class GameViewModel : ViewModel() {
     private var uiState = MutableStateFlow(GameUiState())
     val uiStateFlow: StateFlow<GameUiState>
         get() = uiState.asStateFlow()
+
+    ///why cant i use remember
+    var guessState = mutableStateOf("")
 
     init {
         resetGame()
@@ -36,5 +44,16 @@ class GameViewModel : ViewModel() {
     fun resetGame() {
         usedWords.clear()
         uiState.value = GameUiState(currentScrambledWord = produceRandomWord())
+    }
+
+    fun checkUserGuess() {
+        if (guessState.value == currentWord) {
+            uiState.update { uiStateValue -> uiStateValue.copy(score = uiStateValue.score + 1) }
+
+        } else {
+            uiState.update { uiStateValue ->
+                uiStateValue.copy(isGuessCorrect = false)
+            }
+        }
     }
 }
