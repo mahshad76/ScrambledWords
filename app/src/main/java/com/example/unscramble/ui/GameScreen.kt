62992521e32487c2f5cde042373cc841d89604ca
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2025-06-13 14:53:18 -0400

    the viewmodel instantiation in the composable is corrected

--- app/src/main/java/com/example/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/unscramble/ui/GameScreen.kt
@@ -54,11 +54,12 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.unscramble.R
 import com.example.unscramble.ui.theme.UnscrambleTheme
 
 @Composable
-fun GameScreen(gameViewModel: GameViewModel = GameViewModel()) {
+fun GameScreen(gameViewModel: GameViewModel = viewModel()) {
 
     val gameUiState by gameViewModel.uiStateFlow.collectAsState()
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
